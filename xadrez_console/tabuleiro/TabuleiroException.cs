b62b07d76df1639b0dc24122313b645ac2f26dcf
Gabriel Carvalho <36541377+Gamebielo@users.noreply.github.com>
2021-09-13 09:44:45 -0300

    Custom Excpetion "TabuleiroException"
    - Tratando possíveis erros de posição com exceção personalizada

---
+++ xadrez_console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        // Criando exceção personalizada
+        public TabuleiroException(string msg) : base(msg)
+        {}
+    }
+}
